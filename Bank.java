9bdcabe424442d7a7b26331fa1e3595de20126fa
Dung30ns-Dr0g0ns546 <qaden.childs@gmail.com>
2025-03-25 14:44:05 +0000

    commit

---
+++ Bank.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+        public class Baml implements HasMenu {
+	        Admin admin = new Admin();
+		Customerlist customers = new CustomerList();
+		Scanner input = new Scanner(System.in);
+
+	public static void main(String[] args){
+		new Bank();
+	}
+
+	public Bank(){
+		this.loadCustomers();
+		this.start();
+		this.saveCustomers();
+	}
